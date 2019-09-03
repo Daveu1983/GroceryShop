7f7a3da33326e3cb0ef5763c33aa435b878e5e35
David Underwood <dunderwood13@hotmail.co.uk>
2019-09-03 20:16:03 +0100

    added functionality to stock check

--- src/main/java/GroceryShop/Fruit.java
+++ src/main/java/GroceryShop/Fruit.java
@@ -2,10 +2,14 @@ package GroceryShop;
 
 public class Fruit
 {
-    private int numberOfFruit = 1;
+    private int numberOfFruit;
 
     public int getNumberOfFruit() {
         return numberOfFruit;
+    }
+
+    public void setNumberOfFruit(int numberOfFruit){
+        this.numberOfFruit = numberOfFruit;
     }
 
 }
--- src/test/java/GroceryShop/FruitStockTests.java
+++ src/test/java/GroceryShop/FruitStockTests.java
@@ -1,5 +1,6 @@
 package GroceryShop;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,9 +11,26 @@ public class FruitStockTests
     public void checkFruitIsInStock()
     {
         Fruit fruit = new Fruit();
+        
+        fruit.setNumberOfFruit(1);
 
         int numberOfFruitInStock = fruit.getNumberOfFruit();
 
         assertTrue( numberOfFruitInStock > 0 );
+    }
+
+    @Test
+    public void stockTakeFruit()
+    {
+        int controlFruit = 5;
+
+        Fruit fruit = new Fruit();
+
+        fruit.setNumberOfFruit(5);
+
+        int numberOfFruitInStock = fruit.getNumberOfFruit();
+
+        assertEquals(controlFruit, numberOfFruitInStock);
+
     }
 }
